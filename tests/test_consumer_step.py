854c80ef0cc2f02bbef0c70e0f0a8da861bb672f
Anentropic <ego@anentropic.com>
2017-11-02 17:16:34 +0000

    fix issue with handler listening multiple routes and custom queue name (not possible)

--- tests/test_consumer_step.py
+++ tests/test_consumer_step.py
@@ -105,13 +105,19 @@ def test_get_handlers_with_queue_and_exchange(*mocks):
         def f1(body):
             return None
 
-        @message_handler('my.routing.key2', 'my.queue2', 'my.exchange2')
+        @message_handler('my.routing.key2', 'my.queue2', 'my.exchange1')
         def f2(body):
             return None
 
-        assert len(reg) == 2
+        # can register same queue name on different exchange
+        @message_handler('my.routing.key2', 'my.queue2', 'my.exchange2')
+        def f3(body):
+            return None
+
+        assert len(reg) == 3
         assert f1 is reg[QueueRegistration('my.routing.key1', 'my.queue1', 'my.exchange1')]
-        assert f2 is reg[QueueRegistration('my.routing.key2', 'my.queue2', 'my.exchange2')]
+        assert f2 is reg[QueueRegistration('my.routing.key2', 'my.queue2', 'my.exchange1')]
+        assert f3 is reg[QueueRegistration('my.routing.key2', 'my.queue2', 'my.exchange2')]
 
         step = ec.AMQPRetryConsumerStep(None)
         handlers = step.get_handlers(channel=mock.MagicMock())
@@ -161,6 +167,42 @@ def test_get_handlers_same_queue_name_and_exchange():
         assert len(reg) == 1
 
         assert f1 is reg[QueueRegistration('my.routing.key1', 'custom_queue', 'custom')]
+
+        step = ec.AMQPRetryConsumerStep(None)
+        handlers = step.get_handlers(channel=mock.MagicMock())
+
+        assert len(handlers) == len(reg)
+
+        for handler in handlers:
+            assert isinstance(handler, ec.AMQPRetryHandler)
+            assert len(handler.consumer.queues) == 1
+            assert len(handler.consumer.callbacks) == 1
+            assert isinstance(handler.consumer.callbacks[0], ec.AMQPRetryHandler)
+            key = (handler.routing_key, handler.queue, handler.exchange)
+            assert handler.consumer.callbacks[0].func is reg[key]
+
+
+@override_settings(EXCHANGES={'my.exchange1': {}, 'my.exchange2': {}})
+def test_get_handlers_with_multiple_routes(*mocks):
+    """
+    Should build handlers from tasks decorated with `@message_handler`
+    using the specified routing key, queue and exchange
+    """
+    with mock.patch.object(ec, 'REGISTRY', new=dict()) as reg:
+
+        # custom queue name is not possible with multiple routes, even with named exchange
+        with pytest.raises(InvalidQueueRegistration):
+            @message_handler(['my.routing.key1', 'my.routing.key2'], 'my.queue1', 'my.exchange1')
+            def bad(body):
+                return None
+
+        @message_handler(['my.routing.key1', 'my.routing.key2'])
+        def f1(body):
+            return None
+
+        assert len(reg) == 2
+        assert f1 is reg[QueueRegistration('my.routing.key1', 'my.routing.key1', 'default')]
+        assert f1 is reg[QueueRegistration('my.routing.key2', 'my.routing.key2', 'default')]
 
         step = ec.AMQPRetryConsumerStep(None)
         handlers = step.get_handlers(channel=mock.MagicMock())
